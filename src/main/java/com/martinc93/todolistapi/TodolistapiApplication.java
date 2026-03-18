31d74b02c4c89fcf04c3cd51f91cf8719df829aa
Martín <martin12081993@gmail.com>
2026-03-18 19:56:14 -0300

    initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/martinc93/todolistapi/TodolistapiApplication.java
@@ -0,0 +1,13 @@
+package com.martinc93.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistapiApplication.class, args);
+	}
+
+}
